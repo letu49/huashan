7ba7ab0d2c7c6da535182b28ecba6d90fc8b59dd
eclipse_user <eclipse@letu.com>
2020-11-18 09:11:21 +0800

    commit -m  aaa

---
+++ src/main/java/cn/letu/git/Apple.java
@@ -0,0 +1,9 @@
+package cn.letu.git;
+/** 
+ * @author letu
+ * @version 1.0
+ * 	
+ */
+public class Apple {
+
+}
---
+++ src/test/java/cn/letu/test/GoodTest.java
@@ -0,0 +1,9 @@
+package cn.letu.test;
+/** 
+ * @author letu
+ * @version 1.0
+ * 	
+ */
+public class GoodTest {
+
+}
